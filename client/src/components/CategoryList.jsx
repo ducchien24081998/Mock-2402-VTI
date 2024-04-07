d4f26aca8b1e43c3c716dba8c4d92c4f593ddf15
ducchien24081998 <ducchien24081998@gmail.com>
2024-04-07 17:42:05 +0700

    buoi 2

---
+++ client/src/components/CategoryList.jsx
@@ -0,0 +1,195 @@
+import React from "react";
+
+const CategoryList = () => {
+  return (
+    <div className="  flex flex-col mt-[30px] justify-center items-center w-full h-auto ">
+      <div className="flex justify-center w-4/5 h-auto">
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-1401-1590396470?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Bánh bao</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-1168-1590480600?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn trưa</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-1164-1590572746?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn với nhóm</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center w-4/5 h-auto">
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-auto w-1/10 ">
+          <img
+            src="https://tea-3.lozi.vn/v1/images/resized/category-web-3-1590571350?w=100&type=f"
+            className="w-4/5"
+          />
+          <div>
+            <p>Ăn vặt</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,11 +3,51 @@ import { IoMdArrowDropdown } from "react-icons/io";
 import flagVN from "../assets/flag_vietnam.png";
 const Header = () => {
   return (
-    <div className="flex items-center justify-center w-full h-[50px] ">
+    <div className="text-[12x] flex items-center justify-center w-full h-[50px] ">
       <div className="flex items-center justify-center">
         <img src={flagVN} className="w-[20px] h-[20px]" />
         <p>Vietnamese</p>
         <IoMdArrowDropdown className="w-[25px] h-[25px]" />
+      </div>
+
+      <div className="ml-5">
+        <p>Tải ứng dụng Loship</p>
+      </div>
+
+      <button className="hover:border-red-600 hover:text-red-600 ml-2 px-2 py-1/2 border-2 rounded-lg border-black">
+        <p>IOS</p>
+      </button>
+
+      <button className="hover:border-red-600 hover:text-red-600 ml-2 px-2 py-1/2 border-2 rounded-lg border-black">
+        <p>Android</p>
+      </button>
+
+      <div className="ml-2">
+        <p>- hoặc -</p>
+      </div>
+
+      <div className="hover:cursor-pointer flex items-center justify-center ml-2">
+        <p>+84</p>
+        <IoMdArrowDropdown className="w-[25px] h-[25px]" />
+      </div>
+      <div>
+        <input className="border-2 border-black rounded-md px-1" placeholder="Nhap so dien thoai" />
+      </div>
+
+      <div>
+        <button className="border-2 border-black rounded-md ml-2">Gui link</button>
+      </div>
+
+      <div className="ml-5">
+        <button>Trở thành đối tác Loship</button>
+      </div>
+
+      <div className="ml-5">
+        <button>Đi chợ với Lomart</button>
+      </div>
+
+      <div className="ml-5">
+        <button>Loship Hỏi đáp</button>
       </div>
     </div>
   );
